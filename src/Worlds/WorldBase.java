505b3d5e8b5b196cbb183954d8188644eef33ce4
Fabiola Fonseca Rivera <fabiola.fonseca@upr.edu>
2019-08-28 13:05:53 -0400

    Made grid lines invisible

--- src/Worlds/WorldBase.java
+++ src/Worlds/WorldBase.java
@@ -14,55 +14,56 @@ import java.util.LinkedList;
  */
 public abstract class WorldBase {
 
-    //How many pixels are from left to right
-    //How many pixels are from top to bottom
-    //Must be equal
-    public int GridWidthHeightPixelCount;
+	//How many pixels are from left to right
+	//How many pixels are from top to bottom
+	//Must be equal
+	public int GridWidthHeightPixelCount;
 
-    //automatically calculated, depends on previous input.
-    //The size of each box, the size of each box will be GridPixelsize x GridPixelsize.
-    public int GridPixelsize;
+	//automatically calculated, depends on previous input.
+	//The size of each box, the size of each box will be GridPixelsize x GridPixelsize.
+	public int GridPixelsize;
 
-    public Player player;
+	public Player player;
 
-    protected Handler handler;
-
-
-    public Boolean appleOnBoard;
-    protected Apple apple;
-    public Boolean[][] appleLocation;
+	protected Handler handler;
 
 
-    public Boolean[][] playerLocation;
-
-    public LinkedList<Tail> body = new LinkedList<>();
-
-
-    public WorldBase(Handler handler){
-        this.handler = handler;
-
-        appleOnBoard = false;
+	public Boolean appleOnBoard;
+	protected Apple apple;
+	public Boolean[][] appleLocation;
 
 
-    }
-    public void tick(){
+	public Boolean[][] playerLocation;
+
+	public LinkedList<Tail> body = new LinkedList<>();
 
 
+	public WorldBase(Handler handler){
+		this.handler = handler;
 
-    }
+		appleOnBoard = false;
 
-    public void render(Graphics g){
 
-        for (int i = 0; i <= 800; i = i + GridPixelsize) {
-
-            g.setColor(Color.white);
-            g.drawLine(0, i, handler.getWidth() , i);
-            g.drawLine(i,0,i,handler.getHeight());
-
-        }
+	}
+	public void tick(){
 
 
 
-    }
+	}
+
+	public void render(Graphics g){
+
+		for (int i = 0; i <= 800; i = i + GridPixelsize) {
+
+//			g.setColor(Color.white);
+//			g.drawLine(0, i, handler.getWidth() , i);
+//			g.drawLine(i,0,i,handler.getHeight());
+
+
+		}
+
+
+
+	}
 
 }
